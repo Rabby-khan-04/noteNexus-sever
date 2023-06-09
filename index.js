451f86e07ec537702f95a41a5bba99edf6ea6776
Rabby Khan <ajrabbyK72@gmail.com>
2023-06-09 12:26:16 +0600

    Add API For All Classes

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ async function run() {
       const result = await classCollection.find().toArray();
       res.send(result);
     });
-
+    //
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
