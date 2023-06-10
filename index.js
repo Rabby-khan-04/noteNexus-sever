6f535178b9f18374f1149af01ddc6f26e119679b
Rabby Khan <ajrabbyK72@gmail.com>
2023-06-10 15:44:30 +0600

    Class Deletion API Added

--- index.js
+++ index.js
@@ -284,6 +284,15 @@ async function run() {
       res.send(result);
     });
 
+    // Delete Selected Class
+    app.delete("/delete-class/:id", verifyJWT, async (req, res) => {
+      const email = req.decoded.email;
+      const id = req.params.id;
+      const query = { studentEmail: email, _id: new ObjectId(id) };
+      const result = await savedClassCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
